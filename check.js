561dfba8cbc8337ea826ab2ffa34817b26e83a95
Rafal Wesolowski <wesolowski@nexus-netsoft.com>
2018-12-24 01:41:36 +0100

    change style

--- check.js
+++ check.js
@@ -1,22 +1,26 @@
-
-class User {
+class MovieRating {
     constructor(film) {
         this.film = film;
     }
 
     domUpdater(response) {
-    	console.log(response, response.imdbRating, this.film);
-    	console.log('');
-    	this.film.innerHTML = this.film.innerHTML + ' | ' + response.imdbRating;
+        console.log(response);
+        this.film.outerHTML = this.film.outerHTML + ' ' + this.button(response.imdbRating, response.imdbID);
+    }
+
+    button(rating, imdbId) {
+        return "<a href='https://www.imdb.com/title/"+imdbId+"/' style='"+this.style()+"'>" + rating + "</a>"
+    }
+
+    style() {
+        return "font-weight: bold;text-decoration: none;background: #f2f52d;padding: 1px 10px;border-radius: 5px;display: inline-block;border: none;font-size: 12px;color: #000;"
     }
 }
 
 var filmList2 = document.querySelectorAll("a.PreviewTooltip");
 for (var filmKey in filmList2) {
 
-    if (parseInt(filmKey)) {
-
-
+    if (!isNaN(parseInt(filmKey))) {
 
         var film = filmList2[filmKey];
         var findChar = film.text.indexOf("(");
@@ -25,42 +29,26 @@ for (var filmKey in filmList2) {
 
         var enFilmName = res[res.length - 1];
 
-        var link = 'https://www.omdbapi.com/?t=' + enFilmName + '&apikey=7ab179b0';
-		let user = new User(film);
+        let url = 'https://www.omdbapi.com/?t=' + enFilmName + '&apikey=7ab179b0';
+        let user = new MovieRating(film);
 
-		// var request = new Request(link, {
-		// 	method: 'GET', 
-		// 	mode: 'cors', 
-		// 	redirect: 'follow',
-		// 	headers: new Headers({
-		// 		"Content-Type": "application/json; charset=utf-8"
-		// 	})
-		// });
+        var request = new Request(url, {
+            method: 'GET',
+            mode: 'cors',
+            redirect: 'follow',
+            headers: new Headers({
+                "Content-Type": "application/json; charset=utf-8",
+            })
+        });
 
-		// // Now use it!
-		// fetch(request).then(User.test);
-
-        fetch(link, { headers: { "Content-Type": "application/json; charset=utf-8" } })
-            .then(res => res.json()) // parse response as JSON (can be res.text() for plain response)
+        fetch(request)
+            .then(res => res.json())
             .then(
-response => user.domUpdater(response)
-            	)
+                response => user.domUpdater(response)
+            )
             .catch(err => {
-
                 console.log("sorry, there are no results for your search")
             });
-
-        // fetch(link, { headers: { "Content-Type": "application/json; charset=utf-8" } })
-        //     .then(res => res.json()) // parse response as JSON (can be res.text() for plain response)
-        //     .then(response => {
-
-        //         film.innerHTML = film.innerHTML + response.imdbRating;
-        //         console.log(film.innerHTML, response.imdbRating);
-        //     }, film)
-        //     .catch(err => {
-
-        //         console.log("sorry, there are no results for your search")
-        //     });
     }
 
 }
